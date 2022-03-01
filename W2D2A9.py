1917269f4bf3dea7fd2a95fe43865e8c39975c7e
MTavanamehr <mehrad.tavanamehr@contech.university>
2022-03-01 11:58:27 +0330

    Inforamtion of Assignment

--- W2D2A9.py
+++ W2D2A9.py
@@ -1,37 +1,44 @@
 import psycopg2
 from pydataset import data
-
+import pandas as pd
 
 # load data from dataset to put in table
 df = data('iris')
+df = pd.DataFrame(df)
 
 # create connetion to postgreSQL
 postgresConnection = psycopg2.connect(host="192.168.0.246", port='5432', database="postgres", user="postgres", password="1qazXSW@")
 cursor = postgresConnection.cursor()
 
 # create table in postgreSQL from Python
-name_table = 'iris3'
+name_table = 'iris'
 # use df.info() to find Dtype
-sqlCreateTable = "create table "+name_table+" (SepalLength NUMERIC(3, 3), SepalWidth NUMERIC(3, 3), PetalLength NUMERIC(3, 3), PetalWidth NUMERIC(3, 3), Species varchar(20));"
+sqlCreateTable = """create table """+name_table+""" (SepalLength real, SepalWidth real, PetalLength real, 
+PetalWidth real, Species varchar(20));"""
 cursor.execute(sqlCreateTable)
 postgresConnection.commit()
 
-# insert data to table
-def single_insert(postgresConnection, insert_req):
-    """ Execute a single INSERT request """
-    cursor = postgresConnection.cursor()
-    try:
-        cursor.execute(insert_req)
-        postgresConnection.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print("Error: %s" % error)
-        postgresConnection.rollback()
-        cursor.close()
-        return 1
-    cursor.close()
+# insert all DataFrame information to table
+stmt= "INSERT into "+name_table+" (SepalLength, SepalWidth, PetalLength, PetalWidth, Species) values(%s,%s,%s,%s,%s)"
+tuples = [tuple(x) for x in df.to_numpy()] 
+cursor.executemany(stmt,tuples)
+# https://dev.mysql.com/doc/connector-python/en/connector-python-api-mysqlcursor-executemany.html
 
-for i in df.index:
-    query = """
-    INSERT into """+name_table+"""(SepalLength, SepalWidth, PetalLength, PetalWidth, Species) values(%s,%s,%s,%s,%s);
-    """ % (df['Sepal.Length'], df['Sepal.Width'], df['Petal.Length'], df['Petal.Width'], df['Species'])
-    single_insert(postgresConnection, query)
+# check the information
+select_count = 'select count(*) from '+name_table+''
+cursor.execute(select_count)
+cursor.fetchall()
+
+# load information from database to use -- two way
+## first select directly and execute from table (without index)
+select = 'select * from '+name_table+''
+cursor.execute(select)
+new_df = cursor.fetchall()
+new_df = pd.DataFrame(new_df)
+
+## second use Pandas read_sql engine (with index)
+new_df = pd.read_sql('select * from '+name_table+'',postgresConnection)
+# https://pythontic.com/pandas/serialization/postgresql
+
+# close connection to database
+cursor.close()
